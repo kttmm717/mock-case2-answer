3574614b942e728878337d6f0cae9be5dcf11923
tomomikato <kttmm717@gmail.com>
2025-03-25 12:52:11 +0900

    input src

--- src/database/migrations/2014_10_12_000000_create_users_table.php
+++ src/database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,8 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
+            $table->boolean('admin_status')->default(false);
+            $table->string('attendance_status')->default('勤務外');
             $table->timestamps();
         });
     }
---
+++ src/database/migrations/2025_03_25_115205_create_applications_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateApplicationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('attendance_record_id')->constrained()->cascadeOnDelete();
+            $table->string('approval_status');
+            $table->time('application_date');
+            $table->date('new_date');
+            $table->time('new_clock_in');
+            $table->time('new_clock_out');
+            $table->time('new_break_in')->nullable();
+            $table->time('new_break_out')->nullable();
+            $table->time('new_break2_in')->nullable();
+            $table->time('new_break2_out')->nullable();
+            $table->string('comment');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('applications');
+    }
+}
